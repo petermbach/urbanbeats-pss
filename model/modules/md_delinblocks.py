201d5bc39b13c23b9a9429fe85255210cbdd5acf
petermbach <peterbach@gmail.com>
2018-05-25 10:46:49 +1000

    Added parameter list to delinblocks module

--- model/modules/md_delinblocks.py
+++ model/modules/md_delinblocks.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-@file   main.pyw
+@file   md_delinblocks.py
 @author Peter M Bach <peterbach@gmail.com>
 @section LICENSE
 
 Urban Biophysical Environments and Technologies Simulator (UrbanBEATS)
-Copyright (C) 2012  Peter M. Bach
+Copyright (C) 2018  Peter M. Bach
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -22,34 +22,127 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 __author__ = "Peter M. Bach"
-__copyright__ = "Copyright 2012. Peter M. Bach"
+__copyright__ = "Copyright 2018. Peter M. Bach"
 
 # --- CODE STRUCTURE ---
-#       (1) ...
+#       (1) Class Definition
+#           1.1 __init__() - primarily for parameter list
+#           1.2 run() - the module algorithm
+#           1.3 supplementary functions - all other modularised functions belonging to the module
 # --- --- --- --- --- ---
 
 # --- PYTHON LIBRARY IMPORTS ---
+import math
+import random as rand
+
 import threading
 import os
 import gc
 import tempfile
 
 # --- URBANBEATS LIBRARY IMPORTS ---
+from ubmodule import *
 import model.ublibs.ubdatatypes as ubdata
+import model.progref.ubglobals as ubglobals
 
 
 # --- MODULE CLASS DEFINITION ---
-class DelinBlocks():
-    """ DELINEATION - SPATIAL SETUP MODULE
-    Loads the spatial maps into the model core and processes them either into
-    Blocks or Patch representation for all other modules to use. Also performs
-    spatial connectivity analysis and prepares all input data in a ready-to-use
-    format.
+class DelinBlocks(UBModule):
+    """ SPATIAL SETUP MODULE - Codename: DelinBlocks
+    Loads the spatial maps into the model core and processes them into Blocks. Also performs spatial connectivity
+    analysis and prepares all spatial input data in a ready-to-use format for all other modules. Links loaded data
+    sets into the model.
     """
-
     def __init__(self):
+        """Initialises UBModule followed by full parameter list definition."""
+        UBModule.__init__(self)
         self.name = "Delineation and Spatial Setup Module"
-        pass
+
+        # PARAMETER LIST DEFINITION
+        # (1) Parameters for Input Data
+        self.create_parameter("landuse_map", STRING, "land use map filepath")
+        self.create_parameter("population_map", STRING, "population map filepath")
+        self.create_parameter("elevation_map", STRING, "elevation map filepath")
+        self.create_parameter("landuse_fud", BOOL, "use urban development land use?")
+        self.create_parameter("population_fud", BOOL, "use urban development population?")
+        self.landuse_map = ""
+        self.population_map = ""
+        self.elevation_map = ""
+        self.landuse_fud = 0
+        self.population_fud = 0
+
+        self.create_parameter("employment_map", STRING, "employment map filepath")
+        self.create_parameter("locality_map", STRING, "locality map filepath")
+        self.create_parameter("soil_map", STRING, "soil map filepath")
+        self.create_parameter("employment_fud", BOOL, "use urban development employment?")
+        self.employment_map = ""
+        self.locality_map = ""
+        self.soil_map = ""
+        self.employment_fud = 0
+
+        # (2) Parameters for Geometric Delineation
+        self.create_parameter("geometry_type", STRING, "block or future types of geometry e.g. hex")
+        self.create_parameter("blocksize", DOUBLE, "resolution of the discretisation grid")
+        self.create_parameter("blocksize_auto", BOOL, "determine resolution automatically?")
+        self.create_parameter("neighbourhood", STRING, "type of neighbourhood to use, Moore or vN")
+        self.create_parameter("patchdelin", BOOL, "delineate patches?")
+        self.create_parameter("spatialmetrics", BOOL, "calculate spatial metrics?")
+        self.geometry_type = "BLOCKS"
+        self.blocksize = 500
+        self.blocksize_auto = 0
+        self.neighbourhood = "M"
+        self.patchdelin = 1
+        self.spatialmetrics = 1
+
+        # (3) Parameters for Spatial Context
+        self.create_parameter("include_geopolitical", BOOL, "include geopolitical map?")
+        self.create_parameter("geopolitical_map", STRING, "geopolitical map filepath")
+        self.create_parameter("geopolitical_attref", STRING, "attribute name reference")
+        self.create_parameter("include_suburb", BOOL, "include suburban map?")
+        self.create_parameter("suburban_map", STRING, "suburban map filepath")
+        self.create_parameter("suburban_attref", STRING, "attribute name reference")
+        self.create_parameter("include_watermgmt", BOOL, "include water management boundaries?")
+        self.create_parameter("watermgmt_map", STRING, "water management map fiepath")
+        self.create_parameter("watermgmt_attref", STRING, "attribute name reference")
+        self.include_geopolitical = 0
+        self.geopolitical_map = ""
+        self.geopolitical_attref = ""
+        self.include_suburb = 0
+        self.suburban_map = ""
+        self.suburban_attref = ""
+        self.include_watermgmt = 0
+        self.watermgmt_map = ""
+        self.watermgmt_attref = ""
+
+        self.create_parameter("considerCBD", BOOL, "consider CBD Location?")
+        self.create_parameter("locationOption", STRING, "method for inputting CBD location")
+        self.create_parameter("locationCity", STRING, "city name")
+        self.create_parameter("locationLong", DOUBLE, "CBD Longitude")
+        self.create_parameter("locationLat", DOUBLE, "CBD Latitude")
+        self.create_parameter("marklocation", BOOL, "mark location on output map")
+        self.considerCBD = 0
+        self.locationOption = "S"   # S = Selection, C = coordinates
+        self.locationLong = float(0.0)
+        self.locationLat = float(0.0)
+        self.marklocation = 0
+
+        # NON-VISIBLE PARAMETER LIST
+        self.xllcorner = float(0.0)     # Obtained from the loaded raster data (elevation) upon run-time
+        self.yllcorner = float(0.0)     # Spatial extents of the input map
+
+        # ----- END OF INPUT PARAMETER LIST -----
 
     def run(self):
+        """Contains the main algorithm for the module, links with all other functions thereafter. This is called by
+        UrbanBEATSSim() simulation's scenario manager depending on which scenario is currently being simulated.
+
+        :return: True upon successful completion
+        """
         pass
+
+        return True
+
+    # Additional Module Functions
+    def otherfunctions(self):
+        pass
+        return True
