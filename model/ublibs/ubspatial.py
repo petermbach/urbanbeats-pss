555f4aadc3972cab72ec7e76e386862aa2faa15b
petermbach <peterbach@gmail.com>
2018-06-02 19:09:26 +1000

    Added scripts to now start handling spatial data (after a bloody long day of getting GDAL to work. Note that this uses at least GDAL Version 2.2.2 so please ensure that your Anaconda distribution has GDAL 2.2.2 installed or else you're going to have a long day ahead of you!). Scripts load boundary map, process the EPSG coordinates and return either in the format of [x,y] or [lat, lng] coordinates.

---
+++ model/ublibs/ubspatial.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+@file   ubspatial.py
+@author Peter M Bach <peterbach@gmail.com>
+@section LICENSE
+
+Urban Biophysical Environments and Technologies Simulator (UrbanBEATS)
+Copyright (C) 2018  Peter M. Bach
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Peter M. Bach"
+__copyright__ = "Copyright 2018. Peter M. Bach"
+
+import osgeo.osr as osr
+import osgeo.ogr as ogr
+
+
+def get_epsg(projcs, rootpath):
+    """Uses ancillary/epsg.cfg to retrieve the EPSG code for a given spatial reference, if the coordinate
+    system description is incorrect, it will return None.
+
+    :param projcs: attribute value for the osr.SpatialReference() PROJCS, obtained using .GetAttrValue("PROJCS")
+    :return:
+    """
+    f = open(rootpath+"/ancillary/epsg.cfg", 'r')   # Open the EPSG.cfg file and extract data
+    epsg = None     # Holds the EPSG code
+    for lines in f:
+        epsg_line = lines.rstrip("\n").split(",")   # Prepares the line of data in the cfg file
+        if epsg_line[1] == projcs:      # If the data file line matches the project coordinate system input
+            epsg = int(epsg_line[0])     # Assign the EPSG
+    f.close()   # The function is deliberately written in a way that forgoes the need for an array of EPSG data
+    return epsg
+
+
+def create_coord_transformation_leaflet(inputEPSG):
+    """Creates the coordinate transformation variable for the leaflet map, uses OSR library. The leaflet
+    EPSG code is 4326, which is WGS84, geographic coordinate system.
+
+    :param inputEPSG: the input EPSG code, format int(), specifying the EPSG of the input map. Can use get_epsg()
+    :return: osr.CoordinateTransformation() object
+    """
+    inSpatial = osr.SpatialReference()
+    inSpatial.ImportFromEPSG(inputEPSG)
+
+    outSpatial = osr.SpatialReference()
+    outSpatial.ImportFromEPSG(4326)
+
+    coordTrans = osr.CoordinateTransformation(inSpatial, outSpatial)
+    return coordTrans
+
+
+def get_bounding_polygon(boundaryfile, option, rootpath):
+    """Loads the boundary Shapefile and obtains the coordinates of the bounding polygon, returns as a list of coords.
+
+    :param boundaryfile: full filepath to the boundary shapefile to load
+    :param option: can obtain coordinates either in the input coordinate system or EPSG4326
+    :return: a list() of coordinates in the format []
+    """
+    mapstats = {}
+    driver = ogr.GetDriverByName('ESRI Shapefile')  # Load the shapefile driver and data source
+    datasource = driver.Open(boundaryfile)
+    if datasource is None:
+        print "Could not open shapefile!"
+        return []
+
+    layer = datasource.GetLayer(0)  # Get the first layer, which should be the only layer!
+    xmin, xmax, ymin, ymax = layer.GetExtent()
+    print xmin, xmax, ymin, ymax
+
+    point1 = ogr.Geometry(ogr.wkbPoint)
+    point1.AddPoint(xmin, ymin)
+    point2 = ogr.Geometry(ogr.wkbPoint)
+    point2.AddPoint(xmax, ymax)
+
+    spatialref = layer.GetSpatialRef()
+    inputprojcs = spatialref.GetAttrValue("PROJCS")
+    if inputprojcs is None:
+        print "Warning, no spatial reference defined"
+        return []
+
+    featurecount = layer.GetFeatureCount()
+    print "Total number of features: ", featurecount
+
+    feature = layer.GetFeature(0)
+    geom = feature.GetGeometryRef()
+
+    area =  geom.GetArea() / 1000000.0
+
+    inputepsg = get_epsg(inputprojcs, rootpath)
+
+    if option == "leaflet":
+        coordtrans = create_coord_transformation_leaflet(inputepsg)
+        geom.Transform(coordtrans)
+        point1.Transform(coordtrans)
+        point2.Transform(coordtrans)
+        xmin = point1.GetX()
+        ymin = point1.GetY()
+        xmax = point2.GetX()
+        ymax = point2.GetY()
+        print xmin, xmax, ymin, ymax
+
+    mapstats["xmin"] = xmin
+    mapstats["xmax"] = xmax
+    mapstats["ymin"] = ymin
+    mapstats["ymax"] = ymax
+    mapstats["area"] = area
+    mapstats["centroid"] = [(xmin + xmax)/2.0, (ymin + ymax)/2.0]
+    mapstats["inputEPSG"] = inputepsg
+    mapstats["coordsysname"] = inputprojcs
+    coordinates = []
+    ring = geom.GetGeometryRef(0)
+    points = ring.GetPointCount()
+    for i in range(points):
+        coordinates.append([ring.GetX(i), ring.GetY(i)])
+
+    if option == "leaflet":
+        # Reverse the x, y to form lat, long because in ESRI's case, lat almost always Y and long almost always X
+        for p in range(len(coordinates)):
+            coordinates[p] = [coordinates[p][1], coordinates[p][0]]
+        mapstats["centroid"] = [(ymin + ymax) / 2.0, (xmin + xmax)/2.0]
+    return coordinates, mapstats
+
+
+# TEST SCRIPT - get_bounding_polygon() function
+# MAPPATH = "C:/Users/peter/Documents/TempDocs/Files/Upperdandy/Boundary.shp"
+# coordinates, mapstats = get_bounding_polygon(MAPPATH, "leaflet",
+#                                              "C:/Users/peter/Documents/Coding Projects/UrbanBEATS-PSS")
+# print mapstats
+# print coordinates
+
