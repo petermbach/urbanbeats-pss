fa73c6e2b0baca08f0a32ab32acff2940f34fd25
petermbach <peterbach@gmail.com>
2018-09-12 11:18:16 +0200

    Slight visual change of one of the Main GUIs titles, Scenario Narrative changed to Scenario Information

--- gui/urbanbeatsmaingui.py
+++ gui/urbanbeatsmaingui.py
@@ -1025,7 +1025,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "UrbanBEATS Planning Support Tool"))
-        self.ScenarioView_lbl.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:600;\">Scenario Narrative</span></p></body></html>"))
+        self.ScenarioView_lbl.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:600;\">Scenario Information</span></p></body></html>"))
         self.Project.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
