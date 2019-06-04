2905b0098cb3518c4b8d62675969b7c3475a767f
Martijn Kuller <martijnkuller@gmail.com>
2019-06-04 11:47:59 +1000

    Imported the SWMM source code in the pollution plugin folder. This source code will be modified to include our pollution generation model.

--- plugins/pollution/test.py
+++ plugins/pollution/test.py
@@ -1,42 +1,46 @@
 import psycopg2 as ps
+# import itertools
+# import numpy as np
+import time
 
-blocks = [1, 2]
+start_time = time.time()
 
-# Connect to the database
-con = ps.connect(
-    host="localhost",
-    database="pollution",
-    user="postgres",
-    password="DrKuller4you")
+try:
+    con = ps.connect(
+        host="localhost",
+        database="pollution",
+        user="postgres",
+        password="DrKuller4you")
+except:
+    print("Unable to connect to the database")
 
-# Create a cursor
+
 cur = con.cursor()
 
-#cur.execute("SELECT timestep, time, rainfall FROM rain_series")
-#rows = cur.fetchall()
-# for r in rows:
-#     print(f"timestep {r[0]} time {r[1]} rainfall {r[2]}")
+cur.execute("SELECT rainfall FROM rain_series")
+time_series_list = cur.fetchall()
+flat_list = []
+for sublist in time_series_list:
+    for item in sublist:
+        flat_list.append(item)
+print(flat_list)
 
-for block in blocks:
-    try:
-        table_name = "block_" + str(block)
-        cur.execute(f"CREATE TABLE {table_name} (timestep serial PRIMARY KEY, c_tss float, c_tn float, c_tp float);")
-    except:
-        print(f"I can't create our {table_name} table!")
-    con.commit()
+# for timestep in range(len(time_series_list)):
+#     print(timestep)
 
 # cur.execute("SELECT count(*) FROM rain_series")
 # count = cur.fetchone()[0]
-# rainfall = 0
-# cum_rain = 0
-# for timestep in range(count):
-#     # rainfall_previous = rainfall
-#     cur.execute(f"SELECT rainfall FROM rain_series WHERE timestep = {timestep}")
-#     rainfall = cur.fetchone()[0]
-#     cum_rain += rainfall
-#     cur.execute("INSERT INTO test (timestep, cum_vol) VALUES (%s, %s)", (timestep, cum_rain))
-#
-# con.commit()
 
-# Close the database connection
-con.close()
+# time_series_list = []
+# for timestep in range(count):
+#     cur.execute(f"SELECT rainfall FROM rain_series WHERE timestep = {timestep}")
+#     rainfall = cur.fetchone()
+#     print(timestep)
+#     time_series_list.append(rainfall)
+#
+# print(time_series_list)
+#
+# end_time = time.time()    #records time of simulation end
+# run_time = end_time - start_time   #calculates total simulation time
+# print("Total runtime: %s sec"%(run_time))
+# print("Time elapsed: %s min" % ((end_time - start_time)/60))
