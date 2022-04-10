7eecb0a841c730e8b30bd1cdc0a8446561f34e0c
Peter M Bach <peterbach@gmail.com>
2022-04-10 13:16:14 +0200

    Added and integrated initial mockups of topography and regions mapping modules.

--- gui/mods_master_guis/__init__.py
+++ gui/mods_master_guis/__init__.py
@@ -1,2 +1,4 @@
-__all__ = ["mod_simgrid_guic"]
+__all__ = ["mod_simgrid_guic", "mod_mapregions_guic", "mod_topography_guic"]
 from .mod_simgrid_guic import CreateSimGridLaunch
+from .mod_mapregions_guic import MapRegionsLaunch
+from .mod_topography_guic import MapTopographyLaunch
