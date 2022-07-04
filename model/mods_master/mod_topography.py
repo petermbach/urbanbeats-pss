adc2c241ab59d8f002963889e28f2b73b497dd25
Peter M Bach <peterbach@gmail.com>
2022-07-04 12:13:21 +0200

    Aesthetic amendments topography module.

--- model/mods_master/mod_topography.py
+++ model/mods_master/mod_topography.py
@@ -1,5 +1,5 @@
 r"""
-@file   mod_simgrid.py
+@file   mod_topography.py
 @author Peter M Bach <peterbach@gmail.com>
 @section LICENSE
 
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 __author__ = "Peter M. Bach"
-__copyright__ = "Copyright 2022. Peter M. Bach"
+__copyright__ = "Copyright 2017-2022. Peter M. Bach"
 
 # --- PYTHON LIBRARY IMPORTS ---
 import sys
@@ -142,7 +142,7 @@ class MapTopographyToSimGrid(UBModule):
 
         # TRACKER VARIABLES
         if self.demminmax:
-            lowest_elev = [-9999, 0]         # Tracking the lowest and highest elevation points [elev, ID]
+            lowest_elev = [9999, 0]         # Tracking the lowest and highest elevation points [elev, ID]
             highest_elev = [-9999, 0]
             # Create the asset collection Localities...
 
@@ -170,6 +170,11 @@ class MapTopographyToSimGrid(UBModule):
                 asset.add_attribute("Elev_Avg", self.nodata)
                 asset.add_attribute("Elev_Min", self.nodata)
                 asset.add_attribute("Elev_Max", self.nodata)
+                if self.nodatatask == "STATUS":
+                    asset.add_attribute("Status", 0)
+                else:
+                    exceptions.append(asset)
+                continue
             else:       # Calculate metrics and transfer elevation to asset
                 asset.add_attribute("Elev_Avg", float(mdata.mean()))
                 asset.add_attribute("Elev_Min", float(mdata.min()))
@@ -186,20 +191,26 @@ class MapTopographyToSimGrid(UBModule):
             self.notify("Lowest elevation in ID"+str(lowest_elev[1])+": "+str(lowest_elev[0])+"m")
             self.notify("Highest elevation in ID" + str(highest_elev[1]) + ": " + str(highest_elev[0]) + "m")
 
-        self.notify_progress(80)
+        self.notify_progress(70)
 
         # Interpolate missing items
         if self.nodatatask == "INTERP":
             self.interpolate_missing_elevations(exceptions)
 
+        self.notify_progress(80)
+
         # --- SECTION 3 - Perform DEM Smoothing if requested
         if self.demsmooth:
-            for i in range(self.dempasses):
+            for i in range(int(self.dempasses)):
+                self.notify("Performing Smoothing Pass #"+str(i+1))
                 self.perform_dem_smoothing(griditems)
 
         self.notify_progress(90)
 
         # --- SECTION 4 - Calculate slope and aspect
+        self.notify("Determining slope and aspect")
+
+
 
 
         self.notify("Mapping of elevation data to simulation grid complete")
@@ -210,24 +221,79 @@ class MapTopographyToSimGrid(UBModule):
     # OTHER MODULE METHODS
     # ==========================================
     def interpolate_missing_elevations(self, exceptions):
+        """Interpolates the missing elevation data from its neighbours, iterating maximum number of times."""
         iterations = 0
+        remove_from_list = []
         while iterations < self.maxiter:
             for a in exceptions:
                 nhd = a.get_attribute("Neighbours")
                 avg, min, max = self.get_neighbourhood_elevations(nhd)
+                avg = np.delete(avg, np.where(avg == self.nodata))
+                min = np.delete(min, np.where(min == self.nodata))
+                max = np.delete(max, np.where(max == self.nodata))
+                if len(avg) == 0:
+                    continue
+                a.add_attribute("Elev_Avg", avg.mean())
+                a.add_attribute("Elev_Min", min.mean())
+                a.add_attribute("Elev_Max", max.mean())
+                remove_from_list.append(a)
 
-            iterations += 1
+            # Loop complete, remove all identified exceptions with data now from exceptions list
+            [exceptions.pop(exceptions.index(e)) for e in remove_from_list]
+            remove_from_list = []       # Reset for next while-loop iterations
+            if len(exceptions) == 0:    # If all exceptions have been addressed, break
+                return True
+            else:
+                iterations += 1         # Else continue iterations
+
+        # Loop through remaining exceptions and assign Status 0
+        for a in exceptions:
+            a.add_attribute("Status", 0)
+        return
 
     def get_neighbourhood_elevations(self, nhd):
-        elev_avg = []
-        elev_min = []
-        elev_max = []
+        """Returns min, max and avg elevation of all neighbours in the input list"""
+        elev_avg, elev_min, elev_max = [], [], []
         for i in range(len(nhd)):
-            asset = self.assets.get_asset_with_name(self.assetident+str(id))
+            asset = self.assets.get_asset_with_name(self.assetident+str(nhd[i]))
             elev_avg.append(asset.get_attribute("Elev_Avg"))
             elev_min.append(asset.get_attribute("Elev_Min"))
             elev_max.append(asset.get_attribute("Elev_Max"))
-        return elev_avg, elev_min, elev_max
+        return np.array(elev_avg), np.array(elev_min), np.array(elev_max)
 
     def perform_dem_smoothing(self, assets):
+        """Runs through all items in the assets list and calculates average elevation from it and
+        neighbourhood to smooth the DEM."""
+        update = []
+        for i in range(len(assets)):
+            curasset = assets[i]
+            avg = curasset.get_attribute("Elev_Avg")
+            min = curasset.get_attribute("Elev_Min")
+            max = curasset.get_attribute("Elev_Max")
+
+            if avg == -9999 or curasset.get_attribute("Status") == 0:
+                continue
+
+            avglist, minlist, maxlist = self.get_neighbourhood_elevations(curasset.get_attribute("Neighbours"))
+            avglist = np.delete(avglist, np.where(avglist == self.nodata))
+            avglist = avglist.tolist()
+            avglist = np.array(avglist.append(avg))
+
+            minlist = np.delete(minlist, np.where(minlist == self.nodata))
+            minlist = minlist.tolist()
+            minlist = np.array(minlist.append(min))
+
+            maxlist = np.delete(maxlist, np.where(maxlist == self.nodata))
+            maxlist = maxlist.tolist()
+            maxlist = np.array(maxlist.append(max))
+
+            print(avglist, minlist, maxlist)
+            update.append([curasset, avglist.mean(), minlist.mean(), maxlist.mean()])
+
+        for i in range(len(update)):
+            update[i][0].add_attribute("Elev_Avg", update[i][1])
+            update[i][0].add_attribute("Elev_Min", update[i][2])
+            update[i][0].add_attribute("Elev_Max", update[i][3])
+
+    def calculate_asset_slope_and_aspect(self):
         pass
